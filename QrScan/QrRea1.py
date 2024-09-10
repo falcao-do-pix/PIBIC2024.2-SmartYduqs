e45a806d9d0c11cdc528e2fd471eee63631ebb2c
Jefferson D. Lima <110485317+Jeff181U@users.noreply.github.com>
2024-09-10 18:23:53 -0400

    Standard python files
    Adding the QRcode generator and scanner made with python

---
+++ QrScan/QrRea1.py
@@ -0,0 +1,24 @@
+import cv2
+import datetime
+# initalize the cam
+cap = cv2.VideoCapture(0)
+# initialize the cv2 QRCode detector
+detector = cv2.QRCodeDetector()
+while True:
+    _, img = cap.read()
+    # detect and decode
+    data, bbox, _ = detector.detectAndDecode(img)
+    # check if there is a QRCode in the image
+    if data:
+        a=data
+        break
+    # display the result
+    cv2.imshow("QRCODEscanner", img)    
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+print(a)
+now = datetime.datetime.now()
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
+cap.release()
+cv2.destroyAllWindows()
